811295a11363db1188dbbb7d396ebaca1ec58e1a
Vitaly Bryukhovetsky <bruhovet@list.ru>
2017-08-30 21:09:00 +0300

    переработал функцию просчета времени

--- lot.php
+++ lot.php
@@ -1,4 +1,6 @@
 <?php
+define('SECONDS_IN_DAY', '86400');
+
 date_default_timezone_set('Europe/Moscow');
 
 // ставки пользователей, которыми надо заполнить таблицу
@@ -10,30 +12,49 @@ $bets = [
 ];
 
 /**
-* This function returns a data of the timespamp was created
+* This function returns a data of the timestamp was created
 * or a time between the timestamp and current time.
 *
 * @param int $_ts receive Unix timestamp format data.
 *
 * @return string
 */
-function tsToTimeOrDate($_ts)
+function tsToTimeOrDate(int $_ts)
 {
-    /** @var string $format */
-    $format = 'd.m.y в H:i';
-    /** @var int $delta_ts */
+    /** @array $caseSystemRu[$firstKey][$secondKey] contains words with russion case endings */
+    $caseSystemRu = [
+        'hours' => ['Час', ' час', ' часа', ' часов'],
+        'minutes' => ['Минуту', ' минуту', ' минуты', ' минут']     
+    ];
+    /** @var string $firstKey contains a first keyword for $caseSystemRu array */
+    $firstKey = 'hours';
+    /** @var int $secondKey contains a second keyword for $caseSystemRu */
+    $secondKey = 0;
+    /** @var int $delta_ts contains time interval in seconds */
     $delta_ts = strtotime('now') - $_ts;
-    if ($delta_ts >= 86400) {
-        return date($format, $_ts);
-    } elseif ($delta_ts >= 7200) {
-        $format = 'H Часов назад';
-    } elseif ($delta_ts >= 3600) {
-        return 'Час назад';
-    } else {
-        $format = 'i минут назад';
+    if ($delta_ts >= SECONDS_IN_DAY) {
+        return date('d.m.y в H:i', $_ts);
     }
-    /** @var string $interval */
-    $interval = gmdate($format, $delta_ts);
+    //** @var string $interval contains time interval in Hour(s) or Minute(s) */
+    $interval = gmdate('H', $delta_ts);
+    if ($interval === '00') {
+        $interval = gmdate('i', $delta_ts);
+        $firstKey = 'minutes';
+    }
+    /** @var int $first_number contains first number of $interval */
+    $first_number = (int) $interval[0];
+    /** @var int $last_number contains second number of $interval */
+    $last_number = (int) $interval[1];
+    if ($last_number > 4 || $first_number == 1 || $last_number == 0) {
+        $secondKey = 3;
+    } elseif ($last_number > 1 && $last_number < 5) {
+        $secondKey = 2;
+    } elseif ($interval === '01') {
+        $interval = '00';
+    } else {
+        $secondKey = 1;
+    }
+    $interval .= ($caseSystemRu[$firstKey][$secondKey] . ' назад');
     return ltrim($interval, '0');
 }
 ?>
