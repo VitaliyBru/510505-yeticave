fd2ffe936d1cf047e715989760810bf62a570131
Vitaly Bryukhovetsky <bruhovet@list.ru>
2017-08-31 17:48:23 +0300

    Добавил защиту от xss атак

--- templates/index.php
+++ templates/index.php
@@ -28,7 +28,7 @@
             <select class="lots__select">
                 <option>Все категории</option>
                 <?php foreach ($goods_type as $item_type): ?>
-                    <option><?=$item_type; ?></option>
+                    <option><?=htmlspecialchars($item_type); ?></option>
                 <?php endforeach; ?>
             </select>
         </div>
@@ -39,12 +39,12 @@
                     <img src="<?=$lot ["img_url"]; ?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$lot ["type"]; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot ["item"]; ?></a></h3>
+                    <span class="lot__category"><?=htmlspecialchars($lot ["type"]); ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=htmlspecialchars($lot ["item"]); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$lot ["price"]; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=htmlspecialchars($lot ["price"]); ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             <?=$lot_time_remaining;?>
